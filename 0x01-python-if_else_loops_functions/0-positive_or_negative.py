a2cd972a6fbdb56f6ad1b67faa6dfad59f3c36f0
Otun Abolarinwa <abolarinwaotun@gmail.com>
2022-12-05 08:09:48 -0800

    zero

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -2,9 +2,9 @@
 import random
 number = random.randint(-10, 10)
 if number < 0:
-    print("98 is positive")
+    print("is positive")
 elif number == 0:
-    print("0 is zero")
+    print("is zero")
 else:
-    print("-98 is negative")
+    print("is negative")
 
